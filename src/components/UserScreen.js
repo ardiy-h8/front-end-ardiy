cb4bf2e86824453edb0d3e6b845d6ee6f0a44ee4
riskinputra <riski.n.putra@gmail.com>
2018-02-10 10:34:37 +0700

    handle form

--- src/components/UserScreen.js
+++ src/components/UserScreen.js
@@ -2,11 +2,23 @@ import React, { Component } from 'react'
 import Input, { InputLabel } from 'material-ui/Input'
 import { FormControl, FormHelperText } from 'material-ui/Form'
 import { Paper, Grid, TextField, IconButton, Button, Card } from 'material-ui'
+import Upload from 'material-ui-upload/Upload'
 
 import Navigation from './Navigation'
 import Header from './Header'
 
 class UserScreen extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      title: '',
+      detail: '',
+      cover: '',
+      marker: '',
+      object: ''
+    }
+  }
+
   render () {
     return (
       <div id='coba' style={styles.root}>
@@ -20,6 +32,8 @@ class UserScreen extends Component {
                 helperText='ex: Your Title'
                 fullWidth
                 margin='normal'
+                value={this.state.title}
+                onChange={e => this.setState({ title: e.target.value })}
               />
               <TextField
                 id='detail'
@@ -27,7 +41,29 @@ class UserScreen extends Component {
                 helperText='ex: Description Detail'
                 fullWidth
                 margin='normal'
+                value={this.state.detail}
+                onChange={e => this.setState({ detail: e.target.value })}
               />
+              <div style={styles.bottom}>
+                <input
+                  accept='image/*'
+                  type='file'
+                  id='cover'
+                  name='cover'
+                  style={{ display: 'none' }}
+                  value={this.state.cover}
+                  onChange={e => this.setState({ cover: e.target.value })}
+                />
+                <label htmlFor='cover'>
+                  <Button
+                    variant='raised'
+                    component='span'
+                    syle={styles.button}
+                  >
+                    Cover Image
+                  </Button>
+                </label>
+              </div>
               <div style={styles.bottom}>
                 <input
                   accept='image/*'
@@ -35,6 +71,8 @@ class UserScreen extends Component {
                   id='marker'
                   name='marker'
                   style={{ display: 'none' }}
+                  value={this.state.marker}
+                  onChange={e => this.setState({ marker: e.target.value })}
                 />
                 <label htmlFor='marker'>
                   <Button
@@ -52,6 +90,8 @@ class UserScreen extends Component {
                   id='object'
                   placeholder='object'
                   style={{ display: 'none' }}
+                  value={this.state.object}
+                  onChange={e => this.setState({ object: e.target.value })}
                 />
                 <label htmlFor='object'>
                   <Button
@@ -87,17 +127,21 @@ const styles = {
   root: {
     top: 56,
     position: 'absolute',
-    width: '100%'
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#eee'
   },
   card: {
     maxWidth: '100%',
-    padding: '2em'
+    padding: '2em',
+    height: '100%'
   },
   container: {
     flex: 1,
     justifyContent: 'center',
     top: 56,
-    padding: '2em'
+    padding: '2em',
+    height: '100%'
   },
   button: {
     padding: '0.75em'
